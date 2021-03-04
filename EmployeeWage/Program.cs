2bdecf098afc83ac8156bf7090ffd73e06e717f2
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-03-04 13:23:41 +0530

    [Sanju] ADD : Calculate wages till days or working hrs reached

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,17 +8,18 @@ namespace EmployeeWage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAY = 2;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
         static void Main(string[] args)
         {
             Console.WriteLine("---Welcome to Employee wage Program---");
 
             //Variables declaration
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
-            //Calculating wages
-
-            for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -33,11 +34,11 @@ namespace EmployeeWage
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee wage is : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days is : " +totalWorkingDays+ " " + "Employee Hours is :" +empHrs);
             }
-            Console.WriteLine("Employee's wage monthly is : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total employee wage is : " + totalEmpWage);
 
         }
     }
