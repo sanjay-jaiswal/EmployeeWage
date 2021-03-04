45b930145ef63302bfcf30e7523c8eac556fa3b7
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-03-04 12:54:07 +0530

    [Sanju] ADD : Calculate monthly employee wages

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,31 +8,37 @@ namespace EmployeeWage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAY = 2;
         static void Main(string[] args)
-       {
+        {
             Console.WriteLine("---Welcome to Employee wage Program---");
 
             //Variables declaration
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
             //Calculating wages
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+
+            for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee wage is : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee wage is : " +empWage);
-            Console.ReadKey();
-      }
+            Console.WriteLine("Employee's wage monthly is : " + totalEmpWage);
+
+        }
     }
 }
