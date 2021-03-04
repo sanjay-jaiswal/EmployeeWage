111df105b0ff59d7661d6e849a05d19c590c818e
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-03-04 12:00:15 +0530

    [Sanju] ADDED:UC2 calculate employee wages

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,15 +6,26 @@ namespace EmployeeWage
     {
        static void Main(string[] args)
        {
+            //Constants declaration
             int IS_FULL_TIME = 1;
-            Random random = new Random();
-            int empCheck = random.Next(2);
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables declaration
+            int empHrs = 0;
+            int empWage = 0;
 
-            //Checking emp is present or not
+            //Calculating wages
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-            Console.WriteLine("Employee is Present");
+            {
+                empHrs = 8;
+            }
             else
-            Console.WriteLine("Employee is Absent");
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee wage is : " +empWage);
             Console.ReadKey();
       }
     }
