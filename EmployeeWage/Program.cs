55e2efe6b9cebac29fe1cfac383543dec3386797
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-03-04 12:29:13 +0530

    [Sanju] ADD : Calculated wage using switch case

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,30 +4,31 @@ namespace EmployeeWage
 {
    class Program
     {
-       static void Main(string[] args)
+        //Constants declaration
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        static void Main(string[] args)
        {
-            //Constants declaration
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            Console.WriteLine("---Welcome to Employee wage Program---");
+
             //Variables declaration
             int empHrs = 0;
             int empWage = 0;
-
-            //Calculating wages
             Random random = new Random();
+            //Calculating wages
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wage is : " +empWage);
